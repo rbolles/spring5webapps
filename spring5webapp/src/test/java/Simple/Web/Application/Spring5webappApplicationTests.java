bd9c174cd0c2e297aade27f8826c3c1f400af792
rbolles <43326404+rbolles@users.noreply.github.com>
2021-02-26 15:01:37 -0500

    first commit

---
+++ spring5webapp/src/test/java/Simple/Web/Application/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package Simple.Web.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
